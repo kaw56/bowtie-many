e8ff9a597981a86bfd0e98299bf97056b7ff7641
Katherine <kwhite@mrc-lmb.cam.ac.uk>
2013-07-01 17:18:39 +0100

    Working align many script
    Dave has loads of reads so this may take some time...

--- bowtie_many.sh
+++ bowtie_many.sh
@@ -1,5 +1,5 @@
 #!bin/bash
-# run bowtie on all. Before running this you must have an indexed
+# run bowtie on all (paired end reads). Before running this you must have an indexed
 # transcriptome/genome to compare againsted (look at bowtie2-build)
 
 # a list of directories to look in for the file
@@ -11,8 +11,18 @@ for i in $MY_DIRECTORIES
         count=`ls -1 $i/*.fastq 2>/dev/null | wc -l`
         if [ $count != 0 ]
         then
-            # run fastqc
-            fastqc --noextract $i/*.fastq
+            # get the R1 file
+            R1=`ls $i/*R1_001.fastq`
+
+            # get the R2 file
+            R2=`ls $i/*R2_001.fastq`
+
+            # specify output file
+            out_file="$i.sam"
+
+            # run bowtie2
+            bowtie2 -x ~/Documents/Katherine/Tatiltrus/Transcriptomes/talitrus_trinity_transcriptome -1 $R1 -2 $R2 -S $out_file
+
         else
             echo ".fastq files not found in $i"
         fi
